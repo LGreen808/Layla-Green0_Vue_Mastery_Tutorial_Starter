a701d74084c76464a5bca7e4493ba04bdb6d7827
LGreen808 <526LGreen@frhsd.com>
2026-02-04 03:52:11 +0000

    Event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'These socks are made of 100% cotton, in the United States, and come in two different colors blue and green!',
             image: './assets/images/socks_green.jpg',
@@ -9,12 +10,23 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
             ],
             sizes: [
                 'Small', 'Medium', 'Large'
             ]
+        }
+    }, 
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }, 
+        removeFromCart() {
+            this.cart -= 1
         }
     }
 })
