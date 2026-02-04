e0dc885c3403c441e2894940dabef633147bb05e
LGreen808 <526LGreen@frhsd.com>
2026-02-04 03:28:41 +0000

    List Rendering

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://www.amazon.com/',
             inventory: 100,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue'},
+            ],
+            sizes: [
+                'Small', 'Medium', 'Large'
+            ]
         }
     }
 })
