07d25e480382299e42c71fc0acf01746d9772d5c
LGreen808 <526LGreen@frhsd.com>
2026-02-04 04:09:51 +0000

    Class and Style Binding

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
             description: 'These socks are made of 100% cotton, in the United States, and come in two different colors blue and green!',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.amazon.com/',
+            inStock: false,
             inventory: 100,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
