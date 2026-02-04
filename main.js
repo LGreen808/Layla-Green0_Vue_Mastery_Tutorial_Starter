967870475db66354b50029c8b46df6c5565097ff
LGreen808 <526LGreen@frhsd.com>
2026-02-04 02:48:00 +0000

    Attribute Binding

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'These socks are made of 100% cotton, in the United States, and come in two different colors blue and green!',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.amazon.com/'
         }
     }
 })
