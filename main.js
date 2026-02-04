277d9352ffba9b6338c4ae366dfa8a1a182eac27
LGreen808 <526LGreen@frhsd.com>
2026-02-04 04:33:40 +0000

    Computed Properties

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             description: 'These socks are made of 100% cotton, in the United States, and come in two different colors blue and green!',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             url: 'https://www.amazon.com/',
-            inStock: false,
+         
             inventory: 100,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
             ],
             sizes: [
                 'Small', 'Medium', 'Large'
@@ -23,11 +24,24 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            
         }, 
         removeFromCart() {
             this.cart -= 1
         }
+
+    },
+    computed: {
+         title() {
+            return this.brand + ' ' + this.product
+         },
+         image() {
+            return this.variants[this.selectedVariant].image
+         },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+         }
     }
 })
