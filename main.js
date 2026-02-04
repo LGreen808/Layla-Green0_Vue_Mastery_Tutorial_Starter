d0596ba75c85da58ffdf8b0783b6a3b62212410c
LGreen808 <526LGreen@frhsd.com>
2026-02-04 03:11:27 +0000

    Conditional Rendering

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             product: 'Socks',
             description: 'These socks are made of 100% cotton, in the United States, and come in two different colors blue and green!',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.amazon.com/'
+            url: 'https://www.amazon.com/',
+            inventory: 100,
+            onSale: true
         }
     }
 })
